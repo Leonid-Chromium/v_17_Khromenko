ed8b1968a45928b3f65fbc5d73379644128b1b95
LeonidKhromenko <l.khromenko@gmail.com>
2020-12-30 17:25:17 +0700

    Create_project

---
+++ app/src/main/java/com/example/khromenko_v_17/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.khromenko_v_17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
