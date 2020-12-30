ed84de395a5922ab4fe83c9c112282f734bfd9de
LeonidKhromenko <l.khromenko@gmail.com>
2020-12-30 18:36:56 +0700

    SOS

--- app/src/main/java/com/example/khromenko_v_17/MainActivity2.kt
+++ app/src/main/java/com/example/khromenko_v_17/MainActivity2.kt
@@ -3,13 +3,9 @@ package com.example.khromenko_v_17
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    fun DomClick() {
         setContentView(R.layout.activity_main2)
     }
 }
